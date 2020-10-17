335fe59e747a79cc38e5a02277cc4cac20847468
destro9670 <31510724+destro9670@users.noreply.github.com>
2020-10-17 23:30:59 +0300

    Initial commit

---
+++ src/main/java/com/destro9670/Chat/PigeonChatApplication.java
@@ -0,0 +1,13 @@
+package com.destro9670.Chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destro9670/Chat/PigeonChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destro9670.Chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
